28d9d5e91a9c79e30bb2258c3356052abdc29741
keven klynsman <kevenklynsman2003@gmail.com>
2023-11-27 22:06:37 -0300

    feat: adding the functionality files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { MovieCard } from "../components/MovieCard";
+
+import "./MoviesGrid.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+export const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMoveis = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    console.log(topRatedUrl);
+    getTopRatedMoveis(topRatedUrl);
+  }, []);
+
+  console.log(topMovies);
+
+  return (
+    <div className="conteiner">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-conteiner">
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
